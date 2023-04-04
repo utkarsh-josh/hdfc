e43f0f026c37b240e69dd550870623e2d3c8bcc7
Utkarsh Mukul <utkarsh.mukul@joshsoftware.com>
2023-04-04 14:04:29 +0530

    Removed unused struct

--- spec/response.go
+++ spec/response.go
@@ -2,11 +2,6 @@ package spec
 
 type AddWebsiteResponse bool
 
-/*type GetWebsiteResponse struct{
-	Name string `json:"name"`
-	Status string `json:"status"`
-}*/
-
 type ListWebsitesResponse struct {
 	StatusMap map[string]string `json:"statusMap"`
 }
